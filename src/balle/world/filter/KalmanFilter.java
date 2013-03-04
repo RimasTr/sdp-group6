99515b0c0e4067af5d5d597c45ba9ebb989a642b
RimasTr <rimas.trumpa@gmail.com>
2013-03-04 18:59:52 +0000

    New branch for Kalman filter.

---
+++ src/balle/world/filter/KalmanFilter.java
@@ -0,0 +1,13 @@
+package balle.world.filter;
+
+import balle.world.Snapshot;
+
+public class KalmanFilter implements Filter {
+
+	@Override
+	public Snapshot filter(Snapshot s) {
+		// TODO Auto-generated method stub
+		return s;
+	}
+
+}
