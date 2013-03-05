38889b269ab1a641f8fdd273beb4efd911dd93b9
Rimas Trumpa <rimas.trumpa@gmail.com>
2013-03-05 20:40:36 +0000

    Updating Kalman filter for the opponent and the ball coordinates.

--- src/balle/world/filter/KalmanFilter.java
+++ src/balle/world/filter/KalmanFilter.java
@@ -6,46 +6,51 @@ import Jama.Matrix;
 import balle.world.Coord;
 import balle.world.Snapshot;
 import balle.world.Velocity;
+import balle.world.objects.Ball;
+import balle.world.objects.MovingPoint;
 import balle.world.objects.Robot;
 
 public class KalmanFilter implements Filter {
 
 	// the state of the robot; a vector [x y vx vy]
 	// vx and vy are the components of the velocity
-	public Matrix X;
+	public Matrix[] X = new Matrix[3];
 
 	// the predicted vector state
-	static Matrix Xk1;
+	static Matrix[] Xk1 = new Matrix[3];
 
 	// initial uncertainty matrix, which we will then update => error
 	// covariance matrix
-	static Matrix P = new Matrix(new double[][] { { 5 * 5.0, 0, 0, 0 },
+	static Matrix p = new Matrix(new double[][] { { 5 * 5.0, 0, 0, 0 },
 			{ 0, 5 * 5.0, 0, 0 }, { 0, 0, 1.0, 0 }, { 0, 0, 0, 1.0 } });
+	static Matrix[] P = { p, p, p };
 
 	// predicted estimated covariance matrix - it predicts how much error there
 	// is; it stores the newest estimate of the average error for each part of
 	// the state
-	static Matrix Pk1;
+	static Matrix Pk1[] = new Matrix[3];
 
 	// stores the innovation (measurement residual)
-	static Matrix Vk;
+	static Matrix Vk[] = new Matrix[3];
 	// innovation covariance
-	static Matrix Sk1;
+	static Matrix Sk1[] = new Matrix[3];
 	// optimal kalman gain
-	static Matrix Gain;
+	static Matrix Gain[] = new Matrix[3];
 
 	// time interval
-	double t = 0;
+	double t[] = { 0, 0, 0 };
 
-	double phi = Math.toRadians(-180);
-	double w = Math.toRadians(20.0f);
+	double phi[] = { Math.toRadians(-180), Math.toRadians(-180),
+			Math.toRadians(-180) };
+	double w[] = { Math.toRadians(20.0f), Math.toRadians(20.0f),
+			Math.toRadians(20.0f) };
 
 	/* TODO: set these values appropriately */
 	// time interval
 	static double deltaT = 0.1;
 
 	// previously set to 0.3 and 15.0
-	static double rangeSensorNoise = 0.3f;
+	static double rangeSensorNoise = 0.1f;
 	static double bearingSensorNoise = Math.toRadians(15.0f);
 
 	// these values describe how much noise we believe there is in the model
@@ -55,119 +60,133 @@ public class KalmanFilter implements Filter {
 	static double transitionModelSTDvxy = 0.0f;
 
 	// the Jacobian of the *prediction model
-	static Matrix F = new Matrix(new double[][] { { 1, 0, deltaT, 0 },
+	static Matrix f = new Matrix(new double[][] { { 1, 0, deltaT, 0 },
 			{ 0, 1, 0, deltaT }, { 0, 0, 1, 0 }, { 0, 0, 0, 1 } });
+	static Matrix F[] = { f, f, f };
 
 	// B is the input gain matrix and U is the control input vector
-	static Matrix B, U;
+	static Matrix B[] = new Matrix[3];
+	static Matrix U[] = new Matrix[3];
 
 	// process noise
-	static Matrix Q = new Matrix(new double[][] {
+	static Matrix q = new Matrix(new double[][] {
 			{ transitionModelSTDxy * transitionModelSTDxy, 0, 0, 0 },
 			{ 0, transitionModelSTDxy * transitionModelSTDxy, 0, 0 },
 			{ 0, 0, transitionModelSTDvxy * transitionModelSTDvxy, 0 },
 			{ 0, 0, 0, transitionModelSTDvxy * transitionModelSTDvxy } });
+	static Matrix Q[] = { q, q, q };
 
 	// the Jacobian of the *measurement model
-	static Matrix H;
+	static Matrix H[] = new Matrix[3];;
 
 	// the measurement error matrix
-	static Matrix R = new Matrix(new double[][]{
-			 {rangeSensorNoise*rangeSensorNoise, 0},
-			 {0, bearingSensorNoise*bearingSensorNoise}});
+	static Matrix r = new Matrix(new double[][] {
+			{ rangeSensorNoise * rangeSensorNoise, 0 },
+			{ 0, bearingSensorNoise * bearingSensorNoise } });
+	static Matrix R[] = { r, r, r };
 
 	Random sensorNoise = new Random();
 
-	public void predict() {
-		Xk1 = F.times(X).plus(B.times(U));
-		Pk1 = F.times(P).times(F.transpose()).plus(Q);
-	}
-
-	public void observation(Matrix z) {
-		Vk = z.minus(H.times(Xk1));
-	}
-
-	public void update() {
-
-		// innovation covariance
-		Sk1 = H.times(Pk1).times(H.transpose()).plus(R);
-
-		// compute kalman gain
-		Gain = Pk1.times(H.transpose()).times(Sk1.inverse());
-
-		// update state estimate via Z
-		X = Xk1.plus(Gain.times(Vk));
-
-		// update error covariance
-		Matrix I = Matrix.identity(Pk1.getRowDimension(),
-				Pk1.getColumnDimension());
-		P = (I.minus(Gain.times(H))).times(Pk1);
-	}
-
-
 	public Snapshot filter(Snapshot s) {
 
-		Robot robot = s.getBalle();
+		MovingPoint objects[] = { s.getBalle(), s.getOpponent(), s.getBall() };
 
-		// get state
-		double x = robot.getPosition().x;
-		double y = robot.getPosition().y;
-		
-		double vx = robot.getVelocity().x;
-		double vy = robot.getVelocity().y;
-		
-		X = new Matrix(new double[][] { { x }, { y }, { vx }, { vy } });
-		B = new Matrix(new double[][] { { vx, vy, 0, 0 } }).transpose();
-		U = new Matrix(new double[][] { { deltaT } });
+		for (int object = 0; object < 3; object++) {
 
-		double sqxy = (x * x + y * y);
-		double term00 = -y / sqxy;
-		double term01 = 1 / (x * (1 + (y / x * y / x)));
-		double term10 = x / Math.sqrt(sqxy);
-		double term11 = y / Math.sqrt(sqxy);
+			if (objects[object] == null)
+				continue;
 
-		// the Jacobian of the *measurement model
-		H = new Matrix(new double[][] { { term00, term01, 0, 0 },
-				{ term10, term11, 0, 0 } });
+			// Get current state
+			double x = objects[object].getPosition().x;
+			double y = objects[object].getPosition().y;
+			double vx = objects[object].getVelocity().x;
+			double vy = objects[object].getVelocity().y;
 
-		for (int i = 0; i < 10; i++) {
-			predict();
+			X[object] = new Matrix(new double[][] { { x }, { y }, { vx },
+					{ vy } });
+			B[object] = new Matrix(new double[][] { { vx, vy, 0, 0 } })
+					.transpose();
+			U[object] = new Matrix(new double[][] { { deltaT } });
 
-			// get observations
-			double realBearing = Math.atan2(y, x);
-			double obsBearing = realBearing + bearingSensorNoise
-				* sensorNoise.nextGaussian();
+			double sqxy = (x * x + y * y);
+			double term00 = -y / sqxy;
+			double term01 = 1 / (x * (1 + (y / x * y / x)));
+			double term10 = x / Math.sqrt(sqxy);
+			double term11 = y / Math.sqrt(sqxy);
 
-			double realRange = Math.sqrt(sqxy);
-			double obsRange = Math.max(0.0, realRange + rangeSensorNoise
-				* sensorNoise.nextGaussian());
+			// the Jacobian of the *measurement model
+			H[object] = new Matrix(new double[][] { { term00, term01, 0, 0 },
+					{ term10, term11, 0, 0 } });
 
-			Matrix z = new Matrix(
-					new double[][] { { obsRange }, { obsBearing } });
+			for (int i = 0; i < 10; i++) {
+				// predict
+				Xk1[object] = F[object].times(X[object]).plus(
+						B[object].times(U[object]));
+				Pk1[object] = F[object].times(P[object])
+						.times(F[object].transpose()).plus(Q[object]);
 
-			observation(z);
-			update();
+				// get observations
+				double realBearing = Math.atan2(y, x);
+				double obsBearing = realBearing + bearingSensorNoise
+						* sensorNoise.nextGaussian();
 
-			// update coordinates
-			x += vx + deltaT * (Math.cos(phi) - Math.sin(phi));
-			y += vy + deltaT * (Math.cos(phi) + Math.sin(phi));
-			phi += w * deltaT;
+				double realRange = Math.sqrt(sqxy);
+				double obsRange = Math.max(0.0, realRange + rangeSensorNoise
+						* sensorNoise.nextGaussian());
 
-			vx += 1.0 * deltaT * Math.cos(t);
-			vy += 1.0 * deltaT * Math.cos(t);
-			w -= 0.1 * deltaT * Math.sin(t);
+				Matrix z = new Matrix(new double[][] { { obsRange },
+						{ obsBearing } });
 
-			t += deltaT;
+				// Observation
+				Vk[object] = z.minus(H[object].times(Xk1[object]));
 
+				// Update
+				// innovation covariance
+				Sk1[object] = H[object].times(Pk1[object])
+						.times(H[object].transpose()).plus(R[object]);
+
+				// compute kalman gain
+				Gain[object] = Pk1[object].times(H[object].transpose()).times(
+						Sk1[object].inverse());
+
+				// update state estimate via Z
+				X[object] = Xk1[object].plus(Gain[object].times(Vk[object]));
+
+				// update error covariance
+				Matrix I = Matrix.identity(Pk1[object].getRowDimension(),
+						Pk1[object].getColumnDimension());
+				P[object] = (I.minus(Gain[object].times(H[object])))
+						.times(Pk1[object]);
+
+				// update coordinates
+				x += vx + deltaT
+						* (Math.cos(phi[object]) - Math.sin(phi[object]));
+				y += vy + deltaT
+						* (Math.cos(phi[object]) + Math.sin(phi[object]));
+				phi[object] += w[object] * deltaT;
+
+				vx += 1.0 * deltaT * Math.cos(t[object]);
+				vy += 1.0 * deltaT * Math.cos(t[object]);
+				w[object] -= 0.1 * deltaT * Math.sin(t[object]);
+
+				t[object] += deltaT;
+			}
 		}
 
 		// Robot updatedRobot = new Robot(new Coord(x, y), new Velocity(vx, vy,
 		// deltaT), robot.getAngularVelocity(), robot.getOrientation());
-		Robot updatedRobot = new Robot(new Coord(X.get(0, 0), X.get(1, 0)),
-				new Velocity(X.get(2, 0), X.get(3, 0), deltaT),
-				robot.getAngularVelocity(), robot.getOrientation());
-		Snapshot updatedSnapshot = new Snapshot(s.getWorld(), s.getOpponent(),
-				updatedRobot, s.getBall(), s.getTimestamp(),
+		Robot updatedRobot = new Robot(
+				new Coord(X[0].get(0, 0), X[0].get(1, 0)), new Velocity(
+						X[0].get(2, 0), X[0].get(3, 0), deltaT), s.getBalle()
+						.getAngularVelocity(), s.getBalle().getOrientation());
+		Robot updatedOpponent = new Robot(new Coord(X[1].get(0, 0), X[1].get(1,
+				0)), new Velocity(X[1].get(2, 0), X[1].get(3, 0), deltaT), s
+				.getOpponent().getAngularVelocity(), s.getOpponent()
+				.getOrientation());
+		Ball updatedBall = new Ball(new Coord(X[2].get(0, 0), X[2].get(1, 0)),
+				new Velocity(X[2].get(2, 0), X[2].get(3, 0), deltaT));
+		Snapshot updatedSnapshot = new Snapshot(s.getWorld(), updatedOpponent,
+				updatedRobot, updatedBall, s.getTimestamp(),
 				s.getControllerHistory());
 		// System.out.println("this is X updated" + X.get(0, 0) + " "
 		// + "this is updated in robot" + updatedRobot.getPosition().x
