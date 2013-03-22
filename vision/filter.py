906145e421e3044210a6bd520ba3e65f06dc8581
Rimas Trumpa <rimas.trumpa@gmail.com>
2013-03-22 15:32:09 +0000

    Improving dribbling filter to make it work on the right side of the pitch. The self.dribblingDist value on line 9 should be tested with the actual robot.

--- vision/filter.py
+++ vision/filter.py
@@ -4,13 +4,14 @@ class Filter:
         self.ballIsLost = False
         self.lastBallCoords = [-1, -1, -1]
         self.lastRobotCoords = [-1, -1, -1]
-        self.dribbler = '--'
+        self.dribbler = '-'
         self.minDist = 2000
+        self.dribblingDist = 300 # TODO: test this value
         self.disable = disable
         pass
 
     def change(self, name, x, y, angle):
-        self.coords[name] = (x, y, angle)
+        self.coords[name] = [x, y, angle]
 
     def update(self):
         if self.disable:
@@ -37,7 +38,9 @@ class Filter:
                     self.dribbler = '-'
                     return self.coords
                 self.lastRobotCoords = self.coords[self.dribbler]
-            if (self.dribbler == 'yellow' or self.dribbler == 'blue'):
+                if (self.correct(self.dribbler)):
+                    self.lastBallCoords = self.moveBallToRobot(self.lastBallCoords, self.lastRobotCoords)
+            if (self.correct(self.dribbler)):
                 if (not self.coords[self.dribbler][0] == -1):
                     print (self.dribbler, 'is dribbling!')
                     self.coords['ball'] = self.findBallCoords(self.lastBallCoords, self.lastRobotCoords, self.coords[self.dribbler])
@@ -50,4 +53,22 @@ class Filter:
         x = oldBall[0] - oldRobot[0] + newRobot[0]
         y = oldBall[1] - oldRobot[1] + newRobot[1]
         return [x, y, 0]
+
+    # If the ball is lost, assume it is self.dribblingDist away from the nearest robot.
+    def moveBallToRobot(self, ballCoords, robotCoords):
+        realDist = self.findDist(ballCoords, robotCoords)
+        k = self.dribblingDist*1.0/realDist
+        # relative coordinates:
+        x = robotCoords[0] - ballCoords[0]
+        y = robotCoords[1] - ballCoords[1]
+        # new relative coordinates:
+        x2 = k*x
+        y2 = k*y
+        # new absolute coordinates:
+        ballCoords[0] = robotCoords[0] - x2
+        ballCoords[1] = robotCoords[1] - y2
+        return ballCoords
+
+    def correct(self, robot):
+        return (robot == 'yellow' or robot == 'blue')
 
